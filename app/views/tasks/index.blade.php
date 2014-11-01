21fd97a5f7295ceb2f4042a9a5d46d54122b6690
jesmeral <jorgeesmeral@hotmail.com>
2014-11-01 02:22:29 +0000

    Actualizacion Index tasks asociados con members

--- app/views/tasks/index.blade.php
+++ app/views/tasks/index.blade.php
@@ -47,12 +47,43 @@
 
     
     <dd class="accordion-navigation">
-      <a href="#panel2">Finished Tasks</a>
+      <a href="#panel2">Finished</a>
       <div id="panel2" class="content">
-
-
-      </div>
-    </dd>
+	@foreach($teammembers as $teammember)
+	  <div class="columns large-12">
+	    {{ HTML::linkAction('teammembers.show',$teammember->name . ' ' . $teammember->lastname,[$teammember->id],[]) }}
+	 </div>
+	 <div class="panel callout radius">
+	   @foreach($tasks as $task)
+	     @if ($task->member_id == $teammember->id and $task->statusTask == 1)
+	       <div class="row">
+		 <div class="columns large-11">
+		   <div class="columns large-2">
+		     Task's id: {{{ $task->id }}}
+		   </div>
+		   <div class="columns large-8">
+		     {{ HTML::linkAction('tasks.show',$task->titulo,[$task->id],[]) }}
+		     <br/>
+		     Duration: {{{ $task->duration }}} Hours.
+		     Start Date: {{{ $task->startDate }}}
+		     End Date: {{{ $task->endDate }}}
+		   </div>
+		   <div class="columns large-1">
+		     {{ HTML::linkAction('tasks.edit','Edit',[$task->id],['class'=>'button tiny']) }}
+		   </div>
+		   <div class="columns large-1">
+		     {{ Form::model($tasks, ['route' => ['tasks.destroy', $task->id], 'method' => 'DELETE' ]) }}
+		     {{ Form::button('Destroy', ['type' => 'submit', 'class' => 'tiny alert button'])}}
+		     {{ Form::close() }}
+		   </div>
+	         </div>
+	       </div>
+	     @endif
+	   @endforeach
+	 </div>
+       @endforeach
+     </div>
+   </dd>
 
   </dl>
 <br/>
